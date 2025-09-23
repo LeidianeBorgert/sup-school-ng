6b457502d05e76b4e65dfc8530025888d7e23ae7
leidiane <leidianeborgert@gmail.com>
2025-09-22 22:00:16 -0300

    criado novas paginas

---
+++ src/app/components/calculadora-retangulo/calculadora-retangulo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora-retangulo',
+  imports: [FormsModule],
+  templateUrl: './calculadora-retangulo.component.html',
+  styleUrl: './calculadora-retangulo.component.scss'
+})
+export class CalculadoraRetanguloComponent {
+
+}
