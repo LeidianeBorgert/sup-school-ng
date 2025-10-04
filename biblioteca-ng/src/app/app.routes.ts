4c5a2da286417e1f7de7a03e97188a0320fef4f5
Leidiane Borgert <leidianeborgert@gmail.com>
2025-10-03 22:06:14 -0300

    criado paginas edit e create

--- biblioteca-ng/src/app/app.routes.ts
+++ biblioteca-ng/src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { CategoriaList } from './pages/categorias/list/list';
+import { CategoriaCreate } from './pages/categorias/create/create';
+import { combineLatest } from 'rxjs';
+import { Component } from '@angular/core';
+import { CategoriaEdit } from './pages/categorias/edit/edit';
 
 export const routes: Routes = [
-    { path: "categorias", component: CategoriaList }
+    { path: "categorias", component: CategoriaList },
+    { path: "categorias/cadastrar", component: CategoriaCreate },
+    {path:"categorias/editar/:id", component: CategoriaEdit},
+
+    
 ];
--- biblioteca-ng/src/app/pages/categorias/list/list.ts
+++ biblioteca-ng/src/app/pages/categorias/list/list.ts
@@ -4,14 +4,17 @@ import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { CategoriaService } from '../../../services/categoria.service';
 import { CategoriaResponse } from '../../../models/categoria.dto';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'categoria-list',
-  imports: [ButtonModule, TableModule, CommonModule],
+  imports: [ButtonModule, TableModule, CommonModule, RouterLink],
   templateUrl: './list.html',
   styleUrl: './list.scss'
 })
 export class CategoriaList {
+
   categorias: CategoriaResponse[] = [];
 
   constructor(private categoriaService: CategoriaService) {
@@ -19,11 +22,22 @@ export class CategoriaList {
 
 
   ngOnInit() {
+    this.carregarCategorias();
+
+  }
+
+  private carregarCategorias() {
     this.categoriaService.getAll().subscribe({
       next: categorias => this.categorias = categorias,
       error: erro => alert("Não foi possível carregar as categorias")
-    })
+    });
+  }
 
+  apagar(id: number) {
+    this.categoriaService.delete(id).subscribe({
+      next: categorias => this.carregarCategorias(),
+      error: erro => alert("Não foi possível apagar a categoria")
+    })
   }
 
 }
